d6fb2b330f5554615eb96cd81213d3bdfb0c4a78
Ray Sousa <rsousa@acm.org>
2018-10-30 21:34:25 -0400

    Cleanup

--- ex7.m
+++ ex7.m
@@ -1,31 +1,10 @@
-%% Machine Learning Online Class
-%  Exercise 7 | Principle Component Analysis and K-Means Clustering
+%  K-Means Clustering
 %
-%  Instructions
-%  ------------
-%
-%  This file contains code that helps you get started on the
-%  exercise. You will need to complete the following functions:
-%
-%     pca.m
-%     projectData.m
-%     recoverData.m
-%     computeCentroids.m
-%     findClosestCentroids.m
-%     kMeansInitCentroids.m
-%
-%  For this exercise, you will not need to change any code in this file,
-%  or any other files other than those mentioned above.
 %
 
 %% Initialization
 clear ; close all; clc
 
-%% ================= Part 1: Find Closest Centroids ====================
-%  To help you implement K-Means, we have divided the learning algorithm 
-%  into two functions -- findClosestCentroids and computeCentroids. In this
-%  part, you should complete the code in the findClosestCentroids function. 
-%
 fprintf('Finding closest centroids.\n\n');
 
 % Load an example dataset that we will be using
@@ -46,10 +25,6 @@ fprintf('\n(the closest centroids should be 1, 3, 2 respectively)\n');
 fprintf('Program paused. Press enter to continue.\n');
 pause;
 
-%% ===================== Part 2: Compute Means =========================
-%  After implementing the closest centroids function, you should now
-%  complete the computeCentroids function.
-%
 fprintf('\nComputing centroids means.\n\n');
 
 %  Compute means based on the closest centroids found in the previous part.
@@ -65,12 +40,6 @@ fprintf('   [ 7.119387 3.616684 ]\n\n');
 fprintf('Program paused. Press enter to continue.\n');
 pause;
 
-
-%% =================== Part 3: K-Means Clustering ======================
-%  After you have completed the two functions computeCentroids and
-%  findClosestCentroids, you have all the necessary pieces to run the
-%  kMeans algorithm. In this part, you will run the K-Means algorithm on
-%  the example dataset we have provided. 
 %
 fprintf('\nRunning K-Means clustering on example dataset.\n\n');
 
